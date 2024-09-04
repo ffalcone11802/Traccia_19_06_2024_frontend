7ae40c9a0029e316c8ce42d6b14aeb1f04647d8d
ffalcone11802 <f.falcone3@studenti.poliba.it>
2024-09-04 15:28:12 +0200

    Update AlbumDetails.jsx

--- src/components/AlbumDetails.jsx
+++ src/components/AlbumDetails.jsx
@@ -2,16 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import AlbumTable from "./AlbumTable"
 
-export default function AlbumDetails({updateItem}) {
+export default function AlbumDetails() {
   const [album, setAlbum] = useState(null)
   const {id} = useParams()
 
-  const changeItem = function(i, artwork, subtitle) {
-    updateItem({item: {title: i.title, length: i.length}, artwork: artwork, subtitle: subtitle})
-  }
-
   useEffect(() => {
-    fetch("https://1436c56d-79df-4bf2-9318-7760bdc85522-00-1lwrllm2sopup.janeway.replit.dev/api/collections/"+id, {
+    fetch("https://APP_DOMAIN/api/collections/"+id, {
       method: "get",
       headers: new Headers({
         "Content-Type": "application/json"
@@ -23,7 +19,7 @@ export default function AlbumDetails({updateItem}) {
 
   return(
     <>
-      {album && <AlbumTable changeItem={changeItem} data={album} />}
+      {album && <AlbumTable data={album} />}
     </>
   )
 }
