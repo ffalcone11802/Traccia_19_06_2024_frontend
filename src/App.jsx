5f2140dcec223ce0a412921f6b1e4078716e5a1a
ffalcone11802 <f.falcone3@studenti.poliba.it>
2024-09-04 15:17:41 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import AlbumDetails from './components/AlbumDetails'
+import AudioPlayer from './components/AudioPlayer'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Library from './pages/Library'
+import NoPage from "./pages/NoPage"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+export default function App() {
+  const [item, setItem] = useState(null)
+  
+  const updateItem = (item) => {
+    setItem(item)
+  }
+  
+  return (
+    <BrowserRouter>
+      <div id="app">
+        <Navbar />
+        <main id="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/album/:id" element={<AlbumDetails updateItem={updateItem} />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </main>
+        <AudioPlayer item={item}/>
+      </div>
+    </BrowserRouter>
+  )
+}
